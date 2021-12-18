7433e94e14ce611142b28e81485fd38552af0d98
hungry4capital <6098690+hungry4capital@users.noreply.github.com>
2021-12-18 13:12:43 -0700

    initial commit

---
+++ firebase/config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClgVyptAl5AZzHWLOLIr-8fnnhE0l4zf0",
+  authDomain: "s3-tint.firebaseapp.com",
+  projectId: "s3-tint",
+  storageBucket: "s3-tint.appspot.com",
+  messagingSenderId: "866830665113",
+  appId: "1:866830665113:web:33a2cc81bfbb3cfaa37f08",
+  measurementId: "G-69LMM2FZVV",
+};
+initializeApp(firebaseConfig);
